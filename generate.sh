f41c277aa868229635fbc8a64daae8faa2ea7b62
twopothead <3103204417@qq.com>
2020-07-31 23:18:50 +0800

    auto rename biaoti with filename

--- generate.sh
+++ generate.sh
@@ -5,12 +5,16 @@ if [[ $# -lt 1 ]]; then
         exit  
 else
     python xls2tex.py $1 > contents.txt
+    filename="$(basename $1 .xls)"
     bash handle_special_chars.sh
-    cat ./stsq-tex-template/template/stsq_template_firsthalf.txt ./contents.txt ./stsq-tex-template/template/stsq_template_secondhalf.txt > ./stsq-tex-template/stsq-template.tex
+    #cat ./stsq-tex-template/template/stsq_template_firsthalf.txt ./contents.txt ./stsq-tex-template/template/stsq_template_secondhalf.txt > ./stsq-tex-template/stsq-template.tex
+    sed "s#[^u]biaoti{.*}#\\\biaoti{$filename}#" ./stsq-tex-template/template/stsq_template_firsthalf.txt > ./stsq-tex-template/stsq-template.tex
+    cat ./contents.txt ./stsq-tex-template/template/stsq_template_secondhalf.txt >> ./stsq-tex-template/stsq-template.tex
     cd ./stsq-tex-template/ && make
     pwd
     # mv ./stsq-template.pdf .. && cd ..
-    mv ./stsq-template.pdf ..
+    # mv ./stsq-template.pdf ..
+    mv ./stsq-template.pdf ../$filename.pdf
     # cp ./stsq-template.pdf ../"$1"
 fi
 # bash generate.sh test/781.xls
